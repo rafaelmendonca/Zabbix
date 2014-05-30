3f75e7dce06b8dde8688820d1cc3a045dd5940bc
Rafael Mendonça <rafaelmendonca3f@gmail.com>
2014-05-29 22:32:19 -0300

    Comentário
    Adicionado novos comentários.

--- Discovery/json_zabbix.sh
+++ Discovery/json_zabbix.sh
@@ -2,7 +2,7 @@
 # Rafael Mendonca 
 # Qui Mai 29 17:24:32 BRT 2014
 # 
-# Script para gerar saída no formato JSON para Linux / Unix. 
+# Script para gerar saída no formato JSON 
 # 
 # Variables 
 #
@@ -12,6 +12,7 @@ DIRLOG="/var/log/"
 
 # Begin
 for log in $(${FIND} ${DIRLOG} -type f | egrep -v "*.gz.*|*.[0,1].*" | xargs file | grep -v data$ | awk -F\: '{print $1}'); do
+    # Aramzena o valor no ARRAY.
     logfile="$logfile,"'{"{#LOGFILE}":"'$log'"}'
 done
 echo '{"data":['${logfile#,}' ]}'
